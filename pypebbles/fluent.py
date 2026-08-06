5c04e19169b1e3ecf88335cdd37a61ff4b568342
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2026-08-06 17:30:40 +0400

    Add fluent dictionary

---
+++ pypebbles/fluent.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+from dataclasses import dataclass
+
+
+class FluentDict[ItemT](dict[str, ItemT]):
+    def value_of(self, key: str) -> FluentElement[ItemT]:
+        return FluentElement(self[key])
+
+    def and_a(self, **fields: ItemT) -> FluentDict[ItemT]:
+        return self.with_a(**fields)
+
+    def with_a(self, **fields: ItemT) -> FluentDict[ItemT]:
+        return self.merge(FluentDict[ItemT](fields))
+
+    def merge(self, other: FluentDict[ItemT]) -> FluentDict[ItemT]:
+        return FluentDict[ItemT]({**self, **other})
+
+    def drop(self, *keys: str) -> FluentDict[ItemT]:
+        return self.select(*set(self.keys()).difference(keys))
+
+    def select(self, *keys: str) -> FluentDict[ItemT]:
+        return FluentDict[ItemT]({k: v for k, v in self.items() if k in keys})
+
+
+@dataclass(frozen=True)
+class FluentElement[ItemT]:
+    value: ItemT
+
+    def to[ConvertedT](self, a_type: Callable[[ItemT], ConvertedT]) -> ConvertedT:
+        return a_type(self.value)
+
+    def __str__(self) -> str:  # pragma: no cover
+        return str(self.value)
---
+++ tests/test_fluent_dict.py
@@ -0,0 +1,145 @@
+from typing import Any
+from unittest.mock import ANY
+
+from faker import Faker
+
+from pypebbles.fluent import FluentDict
+
+
+def test_should_construct() -> None:
+    assert FluentDict[Any]().with_a(a=1).and_a(b=2).and_a(c=3) == {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+    }
+
+
+def test_should_extract_value() -> None:
+    assert FluentDict[Any]().with_a(a=1).value_of("a").to(int) == 1
+
+
+def test_should_not_drop_anything(faker: Faker) -> None:
+    key = faker.word()
+
+    updated = FluentDict({key: ANY}).drop()
+
+    assert updated == {key: ANY}
+
+
+def test_should_drop_a_key(faker: Faker) -> None:
+    key = faker.word()
+
+    updated = FluentDict({key: ANY}).drop(key)
+
+    assert updated == {}
+
+
+def test_should_drop_many_keys(faker: Faker) -> None:
+    key1 = faker.word()
+    key2 = faker.word()
+
+    updated = FluentDict({key1: ANY, key2: ANY}).drop(key1, key2)
+
+    assert updated == {}
+
+
+def test_should_not_drop_a_key(faker: Faker) -> None:
+    key1 = faker.word()
+    key2 = faker.word()
+
+    updated = FluentDict({key1: ANY, key2: ANY}).drop(key2)
+
+    assert updated == {key1: ANY}
+
+
+def test_should_not_drop_many_keys(faker: Faker) -> None:
+    key1 = faker.word()
+    key2 = faker.word()
+    key3 = faker.word()
+
+    updated = FluentDict({key1: ANY, key2: ANY, key3: ANY}).drop(key3)
+
+    assert updated == {key1: ANY, key2: ANY}
+
+
+def test_should_not_select_anything(faker: Faker) -> None:
+    key = faker.word()
+
+    updated = FluentDict({key: ANY}).select()
+
+    assert updated == {}
+
+
+def test_should_select_a_key(faker: Faker) -> None:
+    key = faker.word()
+
+    updated = FluentDict({key: ANY}).select(key)
+
+    assert updated == {key: ANY}
+
+
+def test_should_select_many_keys(faker: Faker) -> None:
+    key1 = faker.word()
+    key2 = faker.word()
+
+    updated = FluentDict({key1: ANY, key2: ANY}).select(key1, key2)
+
+    assert updated == {key1: ANY, key2: ANY}
+
+
+def test_should_not_select_a_key(faker: Faker) -> None:
+    key1 = faker.word()
+    key2 = faker.word()
+
+    updated = FluentDict({key1: ANY, key2: ANY}).select(key1)
+
+    assert updated == {key1: ANY}
+
+
+def test_should_not_select_many_keys(faker: Faker) -> None:
+    key1 = faker.word()
+    key2 = faker.word()
+    key3 = faker.word()
+
+    updated = FluentDict({key1: ANY, key2: ANY, key3: ANY}).select(key1)
+
+    assert updated == {key1: ANY}
+
+
+def test_should_add_a_key(faker: Faker) -> None:
+    key = faker.word()
+    value = faker.word()
+
+    updated = FluentDict[Any]({}).with_a(**{key: value})
+
+    assert updated == {key: value}
+
+
+def test_should_merge_empty_fluent_dicts() -> None:
+    assert FluentDict({}).merge(FluentDict({})) == {}
+
+
+def test_should_merge_with_empty_fluent_dict(faker: Faker) -> None:
+    a_fluent_dict = FluentDict({faker.word(): faker.word()})
+
+    result = a_fluent_dict.merge(FluentDict({}))
+
+    assert result == a_fluent_dict
+
+
+def test_should_merge_without_overlap(faker: Faker) -> None:
+    key1 = faker.word()
+    key2 = faker.word()
+
+    result = FluentDict({key1: ANY}).merge(FluentDict({key2: ANY}))
+
+    assert result == {key1: ANY, key2: ANY}
+
+
+def test_should_merge_with_overlap(faker: Faker) -> None:
+    key = faker.word()
+    value = faker.word()
+
+    result = FluentDict({key: faker.word()}).merge(FluentDict({key: value}))
+
+    assert result == {key: value}
