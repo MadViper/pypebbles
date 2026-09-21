0e072a2f29b55882c5f4c61e00c2386489780fb2
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2026-09-21 17:50:45 +0400

    Add Encoder protocol

--- pypebbles/security/domain.py
+++ pypebbles/security/domain.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+from collections.abc import Mapping
 from dataclasses import dataclass
-from typing import Protocol
+from typing import Any, Protocol
+
+from pypebbles import JsonDict
 
 
 class Authority(Protocol):  # pragma: no cover
@@ -16,3 +19,14 @@ class Authority(Protocol):  # pragma: no cover
 class Signature:
     name: str
     value: str
+
+
+class Encoder(Protocol):  # pragma: no cover
+    def __call__(self, payload: Mapping[str, Any]) -> str:
+        pass
+
+    def encode(self, payload: Mapping[str, Any]) -> str:
+        pass
+
+    def decode(self, token: str) -> JsonDict:
+        pass
