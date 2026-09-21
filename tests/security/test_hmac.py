5fa912dab450de1d839d1e447fb674ff627f9d4b
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2026-09-21 17:54:22 +0400

    Split tests

--- tests/security/test_hmac.py
+++ tests/security/test_hmac.py
@@ -3,12 +3,16 @@ from faker import Faker
 from pypebbles.security import Hmac
 
 
-def test_hmac(faker: Faker) -> None:
-    message = faker.sentence()
-
-    key_0 = faker.word()
+def test_hmac_is_different_for_different_keys(faker: Faker) -> None:
     key_1 = faker.word()
     key_2 = faker.word()
+    message = faker.sentence()
 
-    assert Hmac(key_0)(message) == Hmac(key_0)(message)
     assert Hmac(key_1)(message) != Hmac(key_2)(message)
+
+
+def test_hmac_is_the_same_for_the_same_key(faker: Faker) -> None:
+    key = faker.word()
+    message = faker.sentence()
+
+    assert Hmac(key)(message) == Hmac(key)(message)
