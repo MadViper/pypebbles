42c6f9c19cd20e7fb499cf5afc40223db0683ee5
Mikheil Kenchoshvili <MadViper@users.noreply.github.com>
2026-09-21 18:40:23 +0400

    Fail on invalid JWT (#12)

--- tests/security/test_jwt.py
+++ tests/security/test_jwt.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pypebbles.security import JWT
 
 from .conftest import _Fake
@@ -23,3 +25,13 @@ def test_roundtrip(fake: _Fake) -> None:
     payload = fake.payload()
 
     assert JWT(key).decode(JWT(key).encode(payload)) == payload
+
+
+def test_fails_on_invalid_token(fake: _Fake) -> None:
+    key = fake.key()
+    payload = fake.payload()
+
+    token = JWT(key).encode(payload)[1:]
+
+    with pytest.raises(ValueError, match="Invalid JWT token"):
+        JWT(key).decode(token)
