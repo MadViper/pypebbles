c1de65543e900d9960e48bd689e6a7cf6c444b8d
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2026-09-21 18:00:27 +0400

    Extract fake class

--- tests/security/test_hmac.py
+++ tests/security/test_hmac.py
@@ -1,18 +1,39 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+
+import pytest
 from faker import Faker
 
 from pypebbles.security import Hmac
 
 
-def test_hmac_is_different_for_different_keys(faker: Faker) -> None:
-    key_1 = faker.word()
-    key_2 = faker.word()
-    message = faker.sentence()
+def test_hmac_is_the_same_for_the_same_key(fake: _Fake) -> None:
+    key = fake.key()
+    message = fake.message()
+
+    assert Hmac(key)(message) == Hmac(key)(message)
+
+
+def test_hmac_is_different_for_different_keys(fake: _Fake) -> None:
+    key_1 = fake.key()
+    key_2 = fake.key()
+    message = fake.message()
 
     assert Hmac(key_1)(message) != Hmac(key_2)(message)
 
 
-def test_hmac_is_the_same_for_the_same_key(faker: Faker) -> None:
-    key = faker.word()
-    message = faker.sentence()
+@pytest.fixture
+def fake() -> _Fake:
+    return _Fake()
 
-    assert Hmac(key)(message) == Hmac(key)(message)
+
+@dataclass(frozen=True)
+class _Fake:
+    faker: Faker = field(default_factory=Faker)
+
+    def key(self) -> str:
+        return self.faker.word()
+
+    def message(self) -> str:
+        return self.faker.sentence()
