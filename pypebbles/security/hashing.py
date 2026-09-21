d281409c4ea1f0fc134bfb3cbb1e709b38fb0c4e
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2026-09-22 00:06:59 +0400

    Extract class

--- pypebbles/security/hashing.py
+++ pypebbles/security/hashing.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+import base64
 import hmac
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
@@ -6,15 +9,41 @@ import bcrypt
 
 
 class Hasher(ABC):
-    def __call__(self, value: str) -> bytes:
+    def __call__(self, value: str) -> Hash:
         return self.hash(value)
 
     @abstractmethod
-    def hash(self, value: str) -> bytes:
+    def hash(self, value: str) -> Hash:
         pass
 
-    def verify(self, value: str, hashed: bytes) -> bool:
+    def verify(self, value: str, hashed: Hash) -> bool:
         return self.hash(value) == hashed
+
+
+@dataclass(frozen=True)
+class Hash:
+    raw: bytes
+
+    @classmethod
+    def from_text(cls, value: str) -> Hash:
+        return cls(value.encode("utf-8"))
+
+    def text(self) -> str:
+        return self.raw.decode("utf-8")
+
+    @classmethod
+    def from_hex(cls, value: str) -> Hash:
+        return cls(bytes.fromhex(value))
+
+    def hex(self) -> str:
+        return self.raw.hex()
+
+    def b64(self) -> str:
+        return base64.b64encode(self.raw).decode()
+
+    @classmethod
+    def from_b64(cls, value: str) -> Hash:
+        return cls(base64.b64decode(value))
 
 
 @dataclass(frozen=True)
@@ -23,18 +52,20 @@ class Hmac(Hasher):
 
     algorithm: str = "sha256"
 
-    def hash(self, value: str) -> bytes:
-        return hmac.new(
-            key=self.secret.encode("utf-8"),
-            msg=value.encode("utf-8"),
-            digestmod=self.algorithm,
-        ).digest()
+    def hash(self, value: str) -> Hash:
+        return Hash(
+            hmac.new(
+                key=self.secret.encode("utf-8"),
+                msg=value.encode("utf-8"),
+                digestmod=self.algorithm,
+            ).digest()
+        )
 
 
 @dataclass(frozen=True)
 class Bcrypt(Hasher):
-    def hash(self, value: str) -> bytes:
-        return bcrypt.hashpw(value.encode("utf-8"), bcrypt.gensalt())
+    def hash(self, value: str) -> Hash:
+        return Hash(bcrypt.hashpw(value.encode("utf-8"), bcrypt.gensalt()))
 
-    def verify(self, value: str, hashed: bytes) -> bool:
-        return bcrypt.checkpw(value.encode("utf-8"), hashed)
+    def verify(self, value: str, hashed: Hash) -> bool:
+        return bcrypt.checkpw(value.encode("utf-8"), hashed.raw)
